6fa2f5c3a443177af994b562879ecbff6b4b13a4
Kim Drezdon <kim.drezdon@gmail.com>
2024-04-22 18:09:04 -0500

    Add basic structure of calendar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const currentDate = new Date();
+	console.log(currentDate);
+
+	const days = Array(35);
+
+	return (
+		<div className='App'>
+			<h1>Balance Predict</h1>
+			<Calendar>
+				{[...days.keys()].map(day => (
+					<Day key={day} />
+				))}
+			</Calendar>
+		</div>
+	);
+}
+
+function Calendar({ children }) {
+	return <div className='calendar'>{children}</div>;
+}
+
+function Day() {
+	return (
+		<div className='day'>
+			<div className='headers'>
+				<h2>Date</h2>
+				<h3>Balance</h3>
+			</div>
+			<div className='transactions'>
+				<p>Transaction 1</p>
+				<p>Transaction 2</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
